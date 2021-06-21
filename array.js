a0ad5bd1353da40f4c1aa940a87cfaf83b7cab6b
vikhulei <65869915+vikhulei@users.noreply.github.com>
2021-06-21 11:41:43 +0300

    Update array.js

--- array.js
+++ array.js
@@ -42,3 +42,75 @@ const getDepartments = arr => {
 }
 
 console.log(getDepartments(people))
+
+const orders = [
+    { orderId: '123', customerId: '123', deliveryDate: '01-01-2020', delivered: true, items: [
+        { productId: '123', price: 55 },
+        { productId: '234', price: 30 },
+    ]},
+    { orderId: '234', customerId: '234', deliveryDate: '01-02-2020', delivered: false, items: [
+        { productId: '234', price: 30 },
+    ]},
+    { orderId: '345', customerId: '234', deliveryDate: '05-01-2020', delivered: true, items: [
+        { productId: '567', price: 30 },
+        { productId: '678', price: 80 },
+    ]},
+    { orderId: '456', customerId: '345', deliveryDate: '12-01-2020', delivered: true, items: [
+        { productId: '789', price: 12 },
+        { productId: '890', price: 90 },
+    ]},
+        { orderId: '578', customerId: '456', deliveryDate: '12-01-2020', delivered: true, items: [
+        { productId: '901', price: 43 },
+        { productId: '123', price: 55 },
+    ]},
+];
+
+// 1) Get a list of the orders for the customer with the ID 234 that have not been delivered.
+
+const getResults = (arr) => {
+  const result = arr.filter(el => el.orderId==='234')
+  result.forEach(element => {
+      if(element.delivered===false) {
+      let items = element.items
+      items.forEach(el => {
+        console.log(el)
+      })
+    } 
+  });
+}
+
+// 2) Create a new property on each order with the total price of items ordered.
+
+const getResults = (arr) => {
+  return arr.map(value => ({...value, total: value.items.reduce((a,b) => a + b.price, 0)}))
+}
+
+// 3) Have all the orders been delivered?
+
+const getResults = (arr) => {
+  return arr.filter(el => el.delivered===false)
+}
+
+const getResults = (arr) => {
+  return arr.every(el => el.delivered)
+}
+
+// 4) Has the customer with ID '123' made any orders?
+
+const getResults = (arr) => {
+  return arr.filter(el => {
+    if(el.customerId==="123") {
+     return el
+   } else {
+     return false
+   } 
+    })
+}
+
+const getResults = (arr) => {
+  return arr.some(el => el.customerId==="123")
+}
+
+
+
+
